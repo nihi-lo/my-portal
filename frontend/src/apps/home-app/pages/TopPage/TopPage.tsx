ce23cba0618ba27c2204a97bb5e333339a563a12
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-09-26 06:29:40 +0900

    ✨ ホームアプリにアプリ一覧セクションを追加
    標準アプリ(ホームアプリ)を除いたサブアプリをリスト表示し、クリックするとアプリページへ遷移する。

--- frontend/src/apps/home-app/pages/TopPage/TopPage.tsx
+++ frontend/src/apps/home-app/pages/TopPage/TopPage.tsx
@@ -1,10 +1,14 @@
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input, Link } from "@nextui-org/react";
 
+import { metadata as homeAppMetadata } from "@/apps/home-app/App";
 import { Container, HStack, VStack, Section } from "@/components/ui";
+import { useSubAppStore } from "@/stores/useSubAppStore";
 
 import { useTopPage } from "./TopPage.hooks";
 
 const TopPage = (): JSX.Element => {
+  const subAppList = useSubAppStore((state) => state.subAppList);
+
   const {
     state: { inputName, resultText },
     action: { handleGreetClick, handleInputChange },
@@ -23,6 +27,32 @@ const TopPage = (): JSX.Element => {
           </HStack>
           {resultText && <div>{resultText}</div>}
         </VStack>
+      </Section>
+      <Section headingAs="h1" title="アプリ一覧">
+        <ul className="flex flex-col gap-2">
+          {subAppList.reduce<React.ReactNode[]>((acc, app) => {
+            if (app.metadata.id === homeAppMetadata.id) {
+              return acc;
+            }
+
+            return [
+              ...acc,
+              <li key={app.metadata.id}>
+                <Link href={`/apps/${app.metadata.id}`} color="foreground">
+                  <HStack align="center" gap="sm">
+                    <div className="overflow-hidden rounded-large">
+                      <app.metadata.Icon />
+                    </div>
+                    <VStack>
+                      <span className="font-semibold">{app.metadata.title}</span>
+                      <span className="text-default-400">{app.metadata.description}</span>
+                    </VStack>
+                  </HStack>
+                </Link>
+              </li>,
+            ];
+          }, [])}
+        </ul>
       </Section>
     </Container>
   );
