11facb338460ab3b8c7f08ab5a819eb3a781389e
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-09-29 13:42:26 +0900

    ✨ Sectionコンポーネントの機能改修

--- frontend/src/components/ui/layout/Section/Section.tsx
+++ frontend/src/components/ui/layout/Section/Section.tsx
@@ -1,33 +1,32 @@
 import { Divider } from "@nextui-org/react";
 
-import { Heading } from "@/components/ui";
+import { Heading, HStack } from "@/components/ui";
 
 import { type SectionVariantProps, sectionVariants } from "./Section.variants";
 
 interface Props
   extends Omit<React.ComponentPropsWithoutRef<"section">, "className" | "style">,
     SectionVariantProps {
+  endContent?: React.ReactNode;
   headingAs: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  title?: string;
+  title: string;
 }
 
 const Section = (props: Props): JSX.Element => {
-  const { headingAs, title, p, px, py, pt, grow, children, ...otherProps } = props;
+  const { endContent, headingAs, title, p, px, py, pt, grow, children, ...otherProps } = props;
 
   const { base } = sectionVariants({ p, px, py, pt, grow });
 
   return (
     <section className={base()} {...otherProps}>
-      {title && (
-        <>
-          <Heading as={headingAs} className="flex items-center gap-2 text-xl font-semibold">
-            {headingAs === "h2" && <div className="h-4 w-0.5 bg-primary" />}
-            {title}
-          </Heading>
-          <Divider className="my-2" />
-        </>
-      )}
-      {children}
+      <HStack align="center" justify="between" gap="md">
+        <Heading as={headingAs} className="truncate text-xl font-bold">
+          {title}
+        </Heading>
+        <div>{endContent}</div>
+      </HStack>
+      <Divider className="mt-2" />
+      <div className="mt-4">{children}</div>
     </section>
   );
 };
