16587ef1ab72ce2ca8e05d6a2ae75de303ea400f
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-10-01 07:24:53 +0900

    ♻️

---
+++ frontend/src/apps/gakusim-app/pages/TopPage/components/MatchResultSection/MatchResultSection.tsx
@@ -0,0 +1,11 @@
+import { Section } from "@/components/ui";
+
+const MatchResultSection = (): JSX.Element => {
+  return (
+    <Section headingAs="h2" title="対戦結果">
+      <p>まだ対戦していません。</p>
+    </Section>
+  );
+};
+
+export { MatchResultSection };
